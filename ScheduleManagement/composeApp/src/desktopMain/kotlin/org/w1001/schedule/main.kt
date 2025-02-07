bb78aa7fde8954812617b62a3f151d37a77da258
kii1u20 <kii1u20@soton.ac.uk>
2025-02-07 03:27:08 +0200

    Moved the creation of AppViewModel outside of the composable function, as this was creating a weird bug where the window would jump to the left

--- ScheduleManagement/composeApp/src/desktopMain/kotlin/org/w1001/schedule/main.kt
+++ ScheduleManagement/composeApp/src/desktopMain/kotlin/org/w1001/schedule/main.kt
@@ -1,19 +1,19 @@
 package org.w1001.schedule
 
-import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
+val viewModel = AppViewModel()
 fun main() = application {
-    val viewModel = remember { AppViewModel() }
 
     Window(
         onCloseRequest = ::exitApplication,
         title = "ScheduleManagement",
-        state = WindowState(size = DpSize(800.dp, 900.dp)),
+        state = WindowState(size = DpSize(800.dp, 900.dp), placement = WindowPlacement.Floating),
     ) {
         if (viewModel.inMainMenu.value) {
             MainMenu(viewModel)
