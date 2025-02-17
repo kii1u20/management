420082be5573359fa0af25b7cc43b5e19d74eeca
Kristian Ivanov <100206016+kii1u20@users.noreply.github.com>
2025-02-17 16:40:54 +0200

    Publish version 0.1.0

--- ScheduleManagement/composeApp/src/jvmMain/kotlin/org/w1001/schedule/DocumentType.kt
+++
@@ -1,7 +0,0 @@
-package org.w1001.schedule
-
-sealed interface DocumentType {
-    object Schedule1 : DocumentType
-    object Schedule2 : DocumentType
-    // Add more document types here as needed
-}
